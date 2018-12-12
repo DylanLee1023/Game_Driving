0bcb73d74cf97c0a2b5a58b7b15a567b3752aff5
Dylan Li <Dylan20160708@gmail.com>
2018-12-12 18:56:10 -0500

    Add files via upload

---
+++ CrushThatTaxi.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrushThatTaxi : MonoBehaviour {
+
+    public bool Crush = false;
+
+    public GameObject player;
+    PlayerManager pm;
+    public GameObject TaxiRoot;
+    public GameObject Taxi_p;
+    public float speed_min = 0;
+    public float speed_max = 0;
+    public int count_taxi = 0;
+    public int count_taxi_max = 0;
+    public int allPoints = 0;
+    List<Taxi> taxis = new List<Taxi>();
+    
+
+    // Use this for initialization
+    void Start () {
+        if (player != null)
+        {
+            pm = player.GetComponent<PlayerManager>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Crush)
+        {
+            this.createTaxi();
+        }
+
+    }
+
+    void createTaxi()
+    {
+        if (this.count_taxi < this.count_taxi_max)
+        {
+            GameObject g = GameObject.Instantiate(this.Taxi_p, this.TaxiRoot.transform);
+            
+            Taxi t = g.GetComponent<Taxi>();
+            taxis.Add(t);
+            t.speed = Random.Range(this.speed_min,this.speed_max);
+            t.pm = this.pm;
+            t.ct = this;
+            g.transform.localScale *= 2;
+            int randPostion = (int)Random.Range(0, 6);
+            float x = 0.0f;
+            float y = 0.0f;
+            switch (randPostion)
+            {
+                case 1:
+                    x = -5f;
+                    break;
+                case 2:
+                    x = -2.9f;
+                    break;
+                case 3:
+                    x = -0.7f;
+                    break;
+                case 4:
+                    x = 1.5f;
+                    break;
+                case 5:
+                    x = 4f;
+                    break;
+                default:
+                    x = 4f;
+                    break;
+            }
+            randPostion = (int)Random.Range(0, 6);
+            switch (randPostion)
+            {
+                case 1:
+                    y = 2f;
+                    break;
+                case 2:
+                    y = 4f;
+                    break;
+                case 3:
+                    y = 6f;
+                    break;
+                case 4:
+                    y = 8f;
+                    break;
+                case 5:
+                    y = 10f;
+                    break;
+                default:
+                    y = 10f;
+                    break;
+
+            }
+            g.transform.localPosition = new Vector3(x,y,0.0f);
+
+
+            this.count_taxi++;
+
+        }
+        
+
+
+    }
+
+    public void taxiCrushed(int points)
+    {
+        this.allPoints += points;
+        this.count_taxi--;
+
+
+
+    }
+
+    public void taxiReset()
+    {
+        for (int i = 0; i < this.taxis.Count; i++)
+        {
+            int randPostion = (int)Random.Range(0, 6);
+            float x = 0.0f;
+            float y = 0.0f;
+            switch (randPostion)
+            {
+                case 1:
+                    x = -5f;
+                    break;
+                case 2:
+                    x = -2.9f;
+                    break;
+                case 3:
+                    x = -0.7f;
+                    break;
+                case 4:
+                    x = 1.5f;
+                    break;
+                case 5:
+                    x = 4f;
+                    break;
+                default:
+                    x = 4f;
+                    break;
+            }
+            randPostion = (int)Random.Range(0, 6);
+            switch (randPostion)
+            {
+                case 1:
+                    y = 2f;
+                    break;
+                case 2:
+                    y = 4f;
+                    break;
+                case 3:
+                    y = 6f;
+                    break;
+                case 4:
+                    y = 8f;
+                    break;
+                case 5:
+                    y = 10f;
+                    break;
+                default:
+                    y = 10f;
+                    break;
+
+            }
+            this.taxis[i].transform.localPosition = new Vector3(x, y, 0.0f);
+            this.taxis[i].gameObject.SetActive(true);
+        }
+ 
+
+    }
+
+}
---
+++ Taxi.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Taxi : MonoBehaviour {
+
+    public float speed = 0;
+    public int points = 1;
+    Vector3 moveingVec3 = new Vector3(0.0f,0.0f,0.0f);
+    float oldX = 0;
+    public PlayerManager pm;
+    public CrushThatTaxi ct;
+    // Use this for initialization
+    void Start () {
+        oldX = this.transform.localPosition.x;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //moveingVec3.Set(oldX - this.transform.localPosition.x, (speed - pm.speed) * 0.02f,0.0f);
+        //this.transform.position += moveingVec3;
+
+        if (this.gameObject.activeSelf && Vector3.Distance(this.transform.localPosition, this.pm.transform.localPosition) < 0.5f)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    
+}
